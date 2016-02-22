44b4fea1b621c59ee8170e3cc3e1f514c9331e86
jenlockett <jenlockett@users.noreply.github.com>
2016-02-21 19:53:48 -0800

    Update index.php

--- index.php
+++ index.php
@@ -14,25 +14,13 @@
 require 'inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
 $config->titleTag = THIS_PAGE; #Fills <title> tag. If left empty will fallback to $config->titleTag in config_inc.php  
 $config->nav1 = array("aboutus.php"=>"About Us") + $config->nav1; 
-/*
-$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
-$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
-$config->metaRobots = 'no index, no follow';
-$config->loadhead = ''; #load page specific JS
-$config->banner = ''; #goes inside header
-$config->copyright = ''; #goes inside footer
-$config->sidebar1 = ''; #goes inside left side of page
-$config->sidebar2 = ''; #goes inside right side of page
-$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
-$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
-*/
-# END CONFIG AREA ---------------------------------------------------------- 
+
 get_header(); #defaults to theme header or header_inc.php
 ?>
 <div class="wrapper" style="margin-top:.5em;">
 	<h1><?=$config->banner;?></h1>
-	<p><em><?=$config->slogan;?></em></p>
-	<a href=""  class="btn btn-primary btn-lg">Learn more</a>
+
+
 </div>
 <p </p>
 <?php
