9d79b2476af76a42abfc316daf18411a8411edcb
jenlockett <jenlockett@users.noreply.github.com>
2016-02-21 20:12:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,43 @@
  */
  
 require 'inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
-$config->titleTag = THIS_PAGE; #Fills <title> tag. If left empty will fallback to $config->titleTag in config_inc.php  
-$config->nav1 = array("aboutus.php"=>"About Us") + $config->nav1; 
+
+$config->titleTag = "News"
+$config->banner = "Environmental News"; 
+
+# SQL statement
+$sql = "SELECT * FROM ";
 
 get_header(); #defaults to theme header or header_inc.php
+
 ?>
-<div class="wrapper" style="margin-top:.5em;">
+<div class="wrapper">
 	<h1><?=$config->banner;?></h1>
 
 
 </div>
 <p </p>
 <?php
+# connection comes first in mysqli (improved) function
+$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+if(mysqli_num_rows($result) > 0)
+{#records exist - process
+
+	while($row = mysqli_fetch_assoc($result))
+	{# process each row
+         echo '<div align="center">
+                  <a href="' . VIRTUAL_PATH . 'p3/news_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Title']) . '</a>';
+         
+         echo '</div>';
+	}
+	
+}else{#no records
+    echo "<div align=center>There are no matching records.</div>";	
+}
+
+@mysqli_free_result($result);
+
 
 
 //dumpDie($config);
