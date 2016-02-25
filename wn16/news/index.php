b6e61141b219f185d26d3c7ed46c19fd2614a327
Mitch Thompson <thomitchell@gmail.com>
2016-02-24 17:58:49 -0800

    Merge branch 'app_design'

--- wn16/news/index.php
+++ wn16/news/index.php
@@ -21,63 +21,46 @@ $config->titleTag = smartTitle(); #Fills <title> tag. If left empty will fallbac
 $config->metaDescription = smartTitle() . ' - ' . $config->metaDescription; 
 
 # SQL statement 
-$sql = "select * from Category";
+$sql = "select * from Category as c
+inner join Subcategory as s on c.CategoryID = s.CategoryID";
 
 //END CONFIG AREA ---------------------------------------------------------- 
 
 get_header(); #defaults to header_inc.php
 ?>
-<h3 align="center">News Feeds</h3>
-
-<!--
-<p>
-    <h3>News Category 1</h3>
-    <b>News Category 1 description</b>
-    <ul>
-        <li><a href="#">News Sub-Category 1</a></li>
-        <li><a href="#">News Sub-Category 2</a></li>
-        <li><a href="#">News Sub-Category 3</a></li>
-    </ul>
-</p>
-<p>
-    <h3>News Category 2</h3>
-    <b>News Category 2 description</b>
-    <ul>
-        <li><a href="#">News Sub-Category 4</a></li>
-        <li><a href="#">News Sub-Category 5</a></li>
-        <li><a href="#">News Sub-Category 6</a></li>
-    </ul>
-</p>
-<p>
-    <h3>News Category 3</h3>
-    <b>News Category 3 description</b>
-    <ul>
-        <li><a href="#">News Sub-Category 7</a></li>
-        <li><a href="#">News Sub-Category 8</a></li>
-        <li><a href="#">News Sub-Category 9</a></li>
-    </ul>
-</p>
--->
-
+<h3>Environmental News</h3>
 
 <?php
 #IDB::conn() creates a shareable database connection via a singleton class
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
+//create empty array to keep track of Categories
+$categories = [];
 
 if(mysqli_num_rows($result) > 0)
 {#there are records - present data
+    
 	while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
-	   echo '<p>';
-	   echo '<a href="read_feed.php?id=' . $row['CategoryID'] . '">' . $row['Category'] . '</a><br />';
-	   echo 'Description: <b>' . $row['Description'] . '</b><br />';   
+	{# pull data from associative array 
+        
+        if(!in_array($row['CategoryTitle'], $categories))
+        {//if CategoryTitle not in array, add to array to not use again, then echo Title&Description
+           array_push($categories,$row['CategoryTitle']);
+           echo '<p class="news_category">';
+	       echo '<b>' . $row['CategoryTitle'] . '</b><br />';
+           echo 'Description: <b>' . $row['CategoryDescription'] . '</b></p><br />';    
+       }
+        
+        //echo each subcategory under current category   
+       echo '<p class="news_subcategory">';
+       echo '<a href="read_feed.php?id=' . $row['SubcategoryID'] . '">' . $row['SubcategoryTitle'] . '</a><br />';
+      
 	   echo '</p>';
 	}
 }else{#no records
 	echo '<div align="center">Sorry, there are no news feeds that match that category</div>';
 }
-@mysqli_free_result($result);
+
 
 get_footer(); #defaults to footer_inc.php
 ?>
--- wn16/news/read_feed.php
+++ wn16/news/read_feed.php
@@ -31,29 +31,32 @@ if((isset($_GET['id'])) && (int)$_GET['id'] > 0){//good data, process
 
 get_header(); #defaults to header_inc.php
 ?>
-<h3 align="center">News Feed View</h3>
 
 <?php
 
 $myFeed = new Feed($id);
 //dumpDie($myFeed);
 
+echo '<p><a href="index.php">BACK</a></p>';
 get_footer(); #defaults to footer_inc.php
 
 
 class Feed
 {
     public $CategoryID = 0;
-    public $Category = '';
-    public $Description = '';
+    public $CategoryTitle = '';
+    public $CategoryDescription = '';
     
     public function __construct($id)
     {
         $this->CategoryID = (int)$id;
         
         #SQL statement
-        $sql = "select * from Category where CategoryID=$this->CategoryID";
-        
+
+  
+        //$sql = "select * from Subcategory where SubcategoryID=$this->CategoryID";
+        $sql = "select * from Category as c inner join Subcategory as s on c.CategoryID = s.CategoryID where s.SubcategoryID=$this->CategoryID";
+
 
         #IDB::conn() creates a shareable database connection via a singleton class
         $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
@@ -64,24 +67,30 @@ class Feed
             while($row = mysqli_fetch_assoc($result))
             {# pull data from associative array
                 
-                $querySplit = explode(" ", $row['Category']);
-                $query = implode("+", $querySplit);
-                $this->Category = $query;
+              $catPlusSubCat = $row['CategoryTitle'] . ' ' . $row['SubcategoryTitle']; 
+              $querySplit = explode(" ", $catPlusSubCat);
+              $query = implode("+", $querySplit);
+              $this->CategoryTitle = $query;
+             
                 
-                
-                  $request = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=' . $this->Category . '&output=rss';
-                  $response = file_get_contents($request);
-                  $xml = simplexml_load_string($response);
-                  print '<h1>' . $xml->channel->title . '</h1>';
-                  foreach($xml->channel->item as $story)
-                  {
+              $request = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=' . $this->CategoryTitle . '&output=rss';    
+              $response = file_get_contents($request);
+              $xml = simplexml_load_string($response);
+              echo '<h1>' . $xml->channel->title . '</h1>';
+              echo '<p>' . $row['SubcategoryDescription'] . '</p><br />';    
+              foreach($xml->channel->item as $story)
+                 {
+                    echo '<div class="article">';
                     echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
                     echo '<p>' . $story->description . '</p><br /><br />';
+                    echo '</div><!-- end class article -->';
                   }
             }
         }
-        echo '<p><a href="index.php">BACK</a></p>';
     
+    
+    
+        
         @mysqli_free_result($result);
     
     
